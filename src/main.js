7c755e79dc11feb0a01c1690d02e9e3723703a82
Claudia <cmjengelsman@gmail.com>
2020-09-18 15:30:23 +1200

    Change config, import fonts

--- src/main.js
+++ src/main.js
@@ -6,4 +6,9 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  // Add google fonts
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap'
+  })
 }
