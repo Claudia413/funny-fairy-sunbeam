650aaf2c38aaf7600ecf2cb4d78eb63175303e68
Claudia <cmjengelsman@gmail.com>
2020-10-20 12:32:38 +1300

    Add extra copy to homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,10 @@
 <template>
   <Layout>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
     <section class="container">
     <h1>Hi there!</h1>
     <p>
-      This page has not yet received much love and attention. The other pages, which you can access through the menu above, have much more information and features to look at.
+      This page has not yet received much love and attention. The other pages, which you can access through the menu above, have more information and features to look at.
     </p>
     <p>Some features that might be a nice idea for this homepage:</p>
     <ul>
@@ -16,6 +14,12 @@
       <li>Short introduction to different services offered, possibly with a link to the treatments page</li>
       <li>Some frequently asked questions and answers</li>
       <li>A small bar with a warning for covid-19 updates that refers to the new page, could be turned off and on as situation changes</li>
+    </ul>
+    <p>Other ideas that could be nice:</p>
+    <ul>
+      <li>An icon that shows in the tab of the browser instead of the default one</li>
+      <li>Link to Google maps for the location</li>
+      <li>Pictures of inside the practise</li>
     </ul>
     <p>This website has been build with VueJS and Gridsome to make it really fast and is currently hosted on Netlify</p>
     <p>All the content currently on here is put in the code. For easy management by you or someone in the team in the future I would set it up with Prismic.io. This is a content management system that offers a free account with data up till 100gb per month. This is quite generous and Prismic works well with Vue and Gridsome. This would not cost you anything extra in hosting costs and will be plenty of allowed data unless everyone on the South island goes to the website and uses it a lot in the same month. <a href="https://prismic.io/">More info on Prismic here</a></p>
