177e161aa51701e7ba5d1ebbdc164f89161d4802
Claudia <cmjengelsman@gmail.com>
2021-04-11 16:36:35 +1200

    Add separate team page

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,23 +2,29 @@
   <div class="layout">
     <header class="menubar">
       <div class="container header">
-      <g-image src="~/assets/tjplogo.png" width="240" />
-      <nav class="nav" :class="showMobileMenu? 'show' : ''">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-        <g-link class="nav__link" to="/treatments">Treatments</g-link>
-        <g-link class="nav__link" to="/news">News</g-link>
-      </nav>
-      <div class="nav-icon" @click="toggleShowMobileMenu()" :class="showMobileMenu? 'open' : ''" title="toggle mobile menu">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
+        <g-image src="~/assets/tjplogo.png" width="240" />
+        <nav class="nav" :class="showMobileMenu ? 'show' : ''">
+          <g-link class="nav__link" to="/">Home</g-link>
+          <g-link class="nav__link" to="/about/">About</g-link>
+          <g-link class="nav__link" to="/team/">Team</g-link>
+          <g-link class="nav__link" to="/treatments">Services</g-link>
+          <g-link class="nav__link" to="/news">News</g-link>
+        </nav>
+        <div
+          class="nav-icon"
+          @click="toggleShowMobileMenu()"
+          :class="showMobileMenu ? 'open' : ''"
+          title="toggle mobile menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </header>
     <main>
-    <slot/>
+      <slot />
     </main>
     <footer>
       <div class="container">
@@ -44,17 +50,28 @@
           </section>
           <section class="contact">
             <address class="footer-address">
-            <p class="strong">Call us at:</p>
-            <p><a href="tel:033434345" class="phonenumber">(03) 34 34 345</a></p>
-            <p class="strong">Find us at:</p>
-            <p>109 Clarence Street<br/>
-            Riccarton<br/>
-            Christchurch, 8011 <br/>
-            <p>(Corner of Clarence Street and Blenheim Road)</p></address>
+              <p class="strong">Call us at:</p>
+              <p><a href="tel:033434345" class="phonenumber">(03) 34 34 345</a></p>
+              <p class="strong">Find us at:</p>
+              <p>
+                109 Clarence Street<br />
+                Riccarton<br />
+                Christchurch, 8011 <br />
+              </p>
+
+              <p>(Corner of Clarence Street and Blenheim Road)</p>
+            </address>
           </section>
           <section class="location">
-            <a href="https://www.google.com/maps/place/Tower+Junction+Physio/@-43.5368789,172.6020191,15.5z/data=!4m5!3m4!1s0x0:0x9c5693a7da53015f!8m2!3d-43.5368379!4d172.6026916">
-            <g-image src="~/assets/map.jpg" alt="map of the crossing of Blenheim Road and Clarence street marking the location of the practise" class="map-image" width="320" /></a>
+            <a
+              href="https://www.google.com/maps/place/Tower+Junction+Physio/@-43.5368789,172.6020191,15.5z/data=!4m5!3m4!1s0x0:0x9c5693a7da53015f!8m2!3d-43.5368379!4d172.6026916"
+            >
+              <g-image
+                src="~/assets/map.jpg"
+                alt="map of the crossing of Blenheim Road and Clarence street marking the location of the practise"
+                class="map-image"
+                width="320"
+            /></a>
           </section>
         </div>
         <div class="fine-print">
@@ -69,21 +86,20 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      showMobileMenu: false
-    }
+      showMobileMenu: false,
+    };
   },
   methods: {
     toggleShowMobileMenu() {
-      this.showMobileMenu = !this.showMobileMenu
-    }
-  }
-}
+      this.showMobileMenu = !this.showMobileMenu;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
 .layout {
   width: 100%;
   margin: 0;
@@ -100,12 +116,12 @@ export default {
 }
 
 .menubar {
-    background-color:#1e2e4d;
-    z-index: 800;
-    @media screen and(max-width: 768px) {
-      position: sticky;
-      top: 0;
-    }
+  background-color: #1e2e4d;
+  z-index: 800;
+  @media screen and(max-width: 768px) {
+    position: sticky;
+    top: 0;
+  }
 }
 
 .header {
@@ -136,7 +152,7 @@ export default {
       left: 0;
     }
     a {
-    margin: 12px;
+      margin: 12px;
     }
   }
 }
@@ -148,7 +164,7 @@ export default {
   text-decoration: none;
 }
 
-.nav-icon{
+.nav-icon {
   grid-area: toggle;
   width: 36px;
   height: 18px;
@@ -157,16 +173,16 @@ export default {
   -moz-transform: rotate(0deg);
   -o-transform: rotate(0deg);
   transform: rotate(0deg);
-  -webkit-transition: .5s ease-in-out;
-  -moz-transition: .5s ease-in-out;
-  -o-transition: .5s ease-in-out;
-  transition: .5s ease-in-out;
+  -webkit-transition: 0.5s ease-in-out;
+  -moz-transition: 0.5s ease-in-out;
+  -o-transition: 0.5s ease-in-out;
+  transition: 0.5s ease-in-out;
   cursor: pointer;
   display: none;
-   @media only screen and (max-width: 768px) {
-		display: block;
+  @media only screen and (max-width: 768px) {
+    display: block;
     z-index: 950;
-	}
+  }
 }
 
 .nav-icon span {
@@ -181,17 +197,18 @@ export default {
   -moz-transform: rotate(0deg);
   -o-transform: rotate(0deg);
   transform: rotate(0deg);
-  -webkit-transition: .25s ease-in-out;
-  -moz-transition: .25s ease-in-out;
-  -o-transition: .25s ease-in-out;
-  transition: .25s ease-in-out;
+  -webkit-transition: 0.25s ease-in-out;
+  -moz-transition: 0.25s ease-in-out;
+  -o-transition: 0.25s ease-in-out;
+  transition: 0.25s ease-in-out;
 }
 
 .nav-icon span:nth-child(1) {
   top: 0px;
 }
 
-.nav-icon span:nth-child(2),.nav-icon span:nth-child(3) {
+.nav-icon span:nth-child(2),
+.nav-icon span:nth-child(3) {
   top: 9px;
 }
 
@@ -226,7 +243,7 @@ export default {
 }
 
 footer {
-    background-color: #1e2e4d;
+  background-color: #1e2e4d;
 }
 
 .footer {
@@ -238,13 +255,12 @@ footer {
   color: white;
   font-size: 13px;
   .strong {
-      font-weight: bold;
-    }
+    font-weight: bold;
+  }
   .hours {
     p {
       line-height: 1;
     }
-
   }
   .footer-address {
     font-style: normal;
@@ -257,7 +273,7 @@ footer {
     }
   }
   .location {
-    img{
+    img {
       margin-top: 16px;
       max-width: 100%;
     }
@@ -286,12 +302,13 @@ footer {
   @media screen and(max-width: 768px) {
     justify-content: center;
   }
-  p, a {
+  p,
+  a {
     font-size: 12px;
     color: #ffffffaf;
     padding-right: 4px;
     margin-bottom: 4px;
     text-decoration: none;
-    }
   }
+}
 </style>
